88f51f3aebbdb5d4eb2766a289fe1ad1ae965aa6
kubarev.d <dmitry.kubarev@winfinity.live>
2022-04-19 17:46:52 +0300

    rename

---
+++ src/com/Lesson_2/thirdTask.java
@@ -0,0 +1,22 @@
+package com.Lesson_2;
+
+import java.util.Scanner;
+
+class   thirdTask {
+    public static void  main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = scan.nextInt();
+
+        if(a>0){
+            a++;
+        }
+        else if(a<0){
+             a -=2;
+        }
+        else if(a == 0){
+            a=10;
+        }
+        System.out.println(a);
+    }
+}
