5a40221613cfee18a18fcfa5c1a63c0150e1701c
kubarev93 <muhck.93@gmail.com>
2022-04-19 23:29:58 +0300

    new

---
+++ src/com/Lesson_2/eightTask.java
@@ -0,0 +1,10 @@
+package com.Lesson_2;
+class   eightTask {
+    public static void  main (String[] args){
+        int a = 1;
+        for(int i =3; i <=24; i+=3){
+           a = a*2;
+            System.out.println(a);
+        }
+    }
+}
