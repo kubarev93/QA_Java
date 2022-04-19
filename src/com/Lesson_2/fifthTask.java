21e37f0f88c4512180113a298dd221f0ef9d7caa
kubarev.d <dmitry.kubarev@winfinity.live>
2022-04-19 18:09:09 +0300

    rename

---
+++ src/com/Lesson_2/fifthTask.java
@@ -0,0 +1,33 @@
+package com.Lesson_2;
+import java.util.Scanner;
+class   fifthTask {
+    public static void  main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = scan.nextInt();
+        System.out.println("Введите второе число");
+        int b = scan.nextInt();
+        System.out.println("Введите третье число");
+        int c = scan.nextInt();
+        int d = 0;
+        int f = 0;
+
+        if(a>0) {
+            d++;
+        } else {
+            f++;
+        }
+        if(b>0) {
+            d++;
+        } else {
+            f++;
+        }
+        if(c>0) {
+            d++;
+        } else {
+            f++;
+        }
+        System.out.println("Колличество положительных чисел: " + d);
+        System.out.println("Колличество отрицательный чисел: " + f);
+    }
+}
