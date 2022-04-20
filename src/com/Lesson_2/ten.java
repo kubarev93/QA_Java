61c451823545b056fd99872648e126763e870dba
kubarev.d <dmitry.kubarev@winfinity.live>
2022-04-20 18:17:03 +0300

    new

---
+++ src/com/Lesson_2/ten.java
@@ -0,0 +1,12 @@
+package com.Lesson_2;
+class   ten {
+    public static void  main (String[] args){
+        int a = 1;
+        int b = 0;
+        for(int i = 1; i < 99; i += 2){
+            a = a + 2;
+            b = b+a ;
+            System.out.println(b +1);
+        }
+    }
+}
