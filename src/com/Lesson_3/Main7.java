9c89dd92bac032dae790424d985edf55dfeb7014
kubarev93 <44097514+kubarev93@users.noreply.github.com>
2022-04-23 19:08:40 +0300

    Create Main7.java

---
+++ src/com/Lesson_3/Main7.java
@@ -0,0 +1,32 @@
+package com.Lesson_3;
+
+import java.util.Random;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Random Number = new Random();
+        int dlinnaMassiva = 12;
+        int[] array = new int[dlinnaMassiva];
+        int x = 0;
+        for (int i = 0; i < array.length; i++) {
+            int random = Number.nextInt(16);
+            array[x] = random;
+            System.out.print(array[x] + " ");
+            x++;
+        }
+        System.out.println();
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < array.length  ; i++) {
+          if (array[i] >= a ) {
+                a = array[i];
+                b = i;
+            }
+
+        }
+        System.out.println("Наибольшее число : " + a);
+        System.out.println("Индекс ячейки наибольшего числа : " + b);
+
+
+    }
+}
